5306b85216a7382dff1e3151c0b6b1e4a6a8edc7
Valeryia_Kruchak <valeryia_kruchak@epam.com>
2019-04-26 13:28:35 +0300

    app

--- server.js
+++ server.js
@@ -4,5 +4,8 @@ var app = express();
 //setting middleware
 app.use(express.static(__dirname + '/')); //Serves resources from public folder
 app.use('/scripts', express.static(__dirname + '/node_modules/'));
+app.get('*', function(req, res) {
+  res.sendFile(__dirname + '/index.html');
+});
 
 var server = app.listen(5000);
